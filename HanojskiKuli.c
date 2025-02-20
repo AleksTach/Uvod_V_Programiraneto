a3d1a1db7f4f465653a97c1c17300f64aa60eaa1
KameZh <cyberbush720@gmail.com>
2025-02-20 11:45:28 +0200

    notforuse1update2

--- HanojskiKuli.c
+++ HanojskiKuli.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void printMove(int disk, char from, char to) {
-    printf("Moving disk %d from %c to %c\n", disk, from, to);
-    for (int i = 0; i < 100000000; i++);
-}
-
-void hanoi(int n, char from, char aux, char to) {
-    if (n == 1) {
-        printMove(1, from, to);
-        return;
-    }
-    hanoi(n - 1, from, to, aux);
-    printMove(n, from, to);
-    hanoi(n - 1, aux, from, to);
-}
+void pm(int disk, char ot, char kum);
+void h(int n, char ot, char mezd, char kum);
 
 int main() {
     int n;
     printf("Enter the number of disks: ");
     scanf("%d", &n);
     printf("\n");
-    hanoi(n, 'A', 'B', 'C');
+    h(n, 'A', 'B', 'C');
     printf("All disks have been moved successfully!\n");
     return 0;
+}
+
+void h(int n, char ot, char mezd, char kum)
+{
+    if (n == 1) {
+        pm(1, ot, kum);
+        return;
+    }
+    h(n - 1, ot, kum, mezd);
+    pm(n, ot, kum);
+    h(n - 1, mezd, ot, kum);
+}
+
+void pm(int disk, char ot, char kum)
+{
+    printf("Moving disk %d from %c to %c\n", disk, ot, kum);
+    for (int i = 0; i < 100000000; i++);
 }
