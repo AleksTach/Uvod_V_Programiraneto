7538d5c17dcef7a34a331499157329a89b3297a2
KameZh <cyberbush720@gmail.com>
2025-02-25 19:01:13 +0200

    gotemall

--- NfaktorielSFunfciq.c
+++ NfaktorielSFunfciq.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 int fact(int n);
+int fact1(int n);
 
 void main()
 {
@@ -18,4 +19,10 @@ int fact(int n)
     int p=1,i;
     for(i=1;i<=n;i++)p=p*i;
     return p;
+}
+
+int fact1(int n)
+{
+    if(n==0)return 1;
+    else return n*fact1(n-1);
 }
